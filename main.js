8ee32988e6519e56e3655608c9f45d30f80d1fb6
Elias <elias@jwplatt.com>
2021-05-02 22:30:00 -0400

    some more html/css work

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 //JQuery control functions
 $(document).ready(function(){
 
-})
+    $(".url-bar").submit(function() {
+        console.log("form submitted");
+        //TODO do stuff
+    });
+
+});
+
+//Form submit on enter keypress
+$('input').keypress(function(event) {
+    if (event.which == 13) {  //enter key = 13
+        event.preventDefault();
+        $(".url-bar").submit();
+    }
+});
+
+
